bd99ef4553616de7aae48ada9b81d6d475131c14
Robin Kluth <kontakt@kluthr.de>
2023-09-27 20:53:58 +0200

    return any errors during update_cron execution

--- src/scripts/checkCron.php
+++ src/scripts/checkCron.php
@@ -6,13 +6,18 @@ require_once(dirname(__DIR__) . '/include/ABSettings.php');
 
 echo "Checking cron." . PHP_EOL;
 
-$abSettings = new ABSettings();
-list($code, $out) = $abSettings->checkCron();
+try {
+    $abSettings = new ABSettings();
+    list($code, $out) = $abSettings->checkCron();
 
-if ($code == 0) {
-    echo "Checking cron succeeded!" . PHP_EOL;
-} else {
-    echo "Error occurred: " . implode('; ', $out);
+    if ($code == 0) {
+        echo "Checking cron succeeded!" . PHP_EOL;
+    } else {
+        echo "Error occurred: " . implode('; ', $out);
+    }
+} catch (Exception $e) {
+    echo "Exception occurred! => " . print_r($e->getMessage());
+    exit(1);
 }
 
 
