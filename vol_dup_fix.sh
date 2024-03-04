b369212ba1e1a61f3273cbedcbbc029c3542f717
Robin Kluth <kontakt@kluthr.de>
2024-03-04 19:14:16 +0100

    Test script for testers

--- vol_dup_fix.sh
+++ vol_dup_fix.sh
@@ -5,11 +5,15 @@ cd /usr/local/emhttp/plugins/appdata.backup/pages/content
 
 if [ -f settings.php.bak ]
 then
-  echo "Fix already applied!"
+  echo "Fix already applied! Undo changes..."
+  rm settings.php
+  mv settings.php.bak settings.php
   exit 1
 fi
 
+echo "Applying fix..."
+
 mv settings.php settings.php.bak
-wget https://raw.githubusercontent.com/Commifreak/unraid-appdata.backup/volume_dup_check_fix/src/pages/content/settings.php
+wget -q https://raw.githubusercontent.com/Commifreak/unraid-appdata.backup/volume_dup_check_fix/src/pages/content/settings.php
 
 echo "Fix applied! Reload plugin settings page!"
