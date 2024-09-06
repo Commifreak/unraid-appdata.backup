fbf9f4de9eda31c68338b2a5cea5d77e512c7da1
Robin Kluth <kontakt@kluthr.de>
2024-09-06 16:49:29 +0200

    Add a slight visible line between container settings and the `Skip?` dropdown. Fixes #33

--- src/pages/content/settings.php
+++ src/pages/content/settings.php
@@ -605,10 +605,20 @@ HTML;
                 $containerExcludes = implode("\r\n", $containerSetting['exclude']);
 
                 echo <<<HTML
+<style>
+.containerSettingsDt {
+    overflow: hidden;
+    white-space: nowrap
+}
+.containerSettingsDt:after {
+    opacity: 0.1;
+    content: "  _____________________________________________________________________________________________________________________________________________________________________";
+}
+</style>
 <div style="display: none" id="actualContainerSettings_{$container['Name']}">$realContainerSetting</div>
         <dl>
-        <dt><img alt="pic" src='$image' height='16' /> <i title='{$container['Image']}' class='fa fa-info-circle'></i> <abbr title='Click for advanced settings'>{$container['Name']}$plexContainerNameSuffix</abbr> <span id="containerMultiMappingIssue_{$container['Name']}" style="display: none; color: darkorange;">WARN: Multi mapping detected!</span></dt>
-        <dd><label for="{$container['Name']}_skip">Skip?</label>
+        <dt class="containerSettingsDt"><img alt="pic" src='$image' height='16' /> <i title='{$container['Image']}' class='fa fa-info-circle'></i> <abbr title='Click for advanced settings'>{$container['Name']}$plexContainerNameSuffix</abbr> <span id="containerMultiMappingIssue_{$container['Name']}" style="display: none; color: darkorange;">WARN: Multi mapping detected!</span></dt>
+        <dd><label for="{$container['Name']}_skip">&nbsp;&nbsp;Skip?</label>
         <select name="containerSettings[{$container['Name']}][skip]" id="{$container['Name']}_skip" data-setting="{$containerSetting['skip']}">
             <option value="no">No</option>
             <option value="yes">Yes</option>
